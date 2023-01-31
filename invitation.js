775305956965803d973454227bb5bff2e6df553e
Kosuke20020715 <111882170+Kosuke20020715@users.noreply.github.com>
2023-01-31 21:29:10 +0900

    Add files via upload

---
+++ invitation.js
@@ -0,0 +1,8 @@
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。
+    return false;//リンク自体の無効化
+});
+
